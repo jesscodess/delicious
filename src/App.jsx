6337c534d69874b92b0ebe96187aaeed7b5d9a17
Jéssica Martins <jesscodes@proton.me>
2025-11-29 02:39:10 -0300

    First commit. Scaffolded project and created file structure. Worked on the "Popular" component.
    Primeiro commit.
    Projeto iniciado e estrutura de arquivos criada. Trabalhei no componente "Popular".

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './pages/Home'
+import Pages from './pages/Pages'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <Pages />
+      </div>
+    </>
+  )
+}
+
+export default App
