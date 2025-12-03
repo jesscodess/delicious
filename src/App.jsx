035f7a562bac2d5088d79883baecf22397fce28c
Jéssica Martins <jesscodes@proton.me>
2025-12-03 01:24:35 -0300

    Worked on the Veggie and Cuisine components.
    Trabalhei com os componentes Veggie e Cuisine.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Home from './pages/Home'
 import Pages from './pages/Pages'
+import Category from './components/Category'
+import { BrowserRouter } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-        <Pages />
+      <div className="App">
+        <BrowserRouter>
+          <Category />
+          <Pages />
+        </BrowserRouter>
       </div>
     </>
   )
